6c3780deb901070eaaddb2387182e8376ea66a50
Emanoel Alves <joao.silva3984@aluno.ce.gov.br>
2025-10-14 13:43:19 +0000

    concluido

--- 4-bimestre/ex02-loop-do-while/index.js
+++ 4-bimestre/ex02-loop-do-while/index.js
@@ -1,4 +1,16 @@
 // CRIE UMA LÓGICA AQUI
+let conta = 5 
+let resultado = conta
+do { 
+    console.log(conta)
+    conta++
+    resultado++
+}while(conta < 10)
+console.log(`${resultado}`)
+
+
+
+
 
 // === NÃO MODIFIQUE NADA ABAIXO ============
 module.exports = resultado
