0c4409a1749430448f7aff74a4a256e783b67fe2
Emanoel Alves <joao.silva3984@aluno.ce.gov.br>
2025-10-22 00:21:47 +0000

    projeto feito

--- 4-bimestre/projetos/projeto5-sistema-de-controle-de-producao/index.js
+++ 4-bimestre/projetos/projeto5-sistema-de-controle-de-producao/index.js
@@ -2,3 +2,42 @@
 // Comando: npm i prompt-sync; npm i -D jest
 
 // CRIE UMA LÓGICA ABAIXO
+const prompt = require("prompt-sync")()
+let diaAtual = 0
+let metaTurno
+let continuarTurno
+let totalPecas = 0
+let resumoDias = ""
+
+do {
+    do {
+        metaTurno = parseInt(prompt("Quantas peças devem ser produzidas nesse turno?: "))
+    } while (typeof metaTurno !== 'number' || metaTurno !== metaTurno)
+
+    let producaoAtual = 0
+    while (producaoAtual < metaTurno) {
+        producaoAtual++
+        console.log(`Peça ${producaoAtual}, produzida com sucesso`)
+    }
+    if (producaoAtual === metaTurno) {
+        console.log("Meta alcançada!")
+    } else {
+        console.log("Meta não alcançada!")
+    }
+    for (let x = 1; x > 0; x--) {
+        diaAtual++
+        const linhaDia = `Dia ${diaAtual} foi produzido ${metaTurno} \n`
+        resumoDias += linhaDia
+        totalPecas += metaTurno
+    }
+    do {
+        continuarTurno = prompt("Deseja simular outro turno? S/N: ").toUpperCase()
+    } while (continuarTurno === "" || continuarTurno === null)
+
+} while (continuarTurno == "S")
+
+console.log("Encerrando sistema de produção")
+console.log("Resumo dos dias anteriores:")
+console.log(resumoDias)
+console.log(`Total geral: ${totalPecas} peças fabricadas!`)
+
